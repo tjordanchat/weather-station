96e2eb2bd11a73519c5cb8b55c7ef882b2e2c824
James Jordan <jamestjordan@gmail.com>
2019-02-05 17:27:46 -0500

    Tue Feb  5 17:18:37 EST 2019

--- launch.sh
+++ launch.sh
@@ -20,6 +20,7 @@ curl -k https://api.usno.navy.mil/imagery/moon.png > phase.png
 ./generate_humid_line.sh
 ./generate_wind_line.sh
 #./calc_moonrise.sh
+sed -i "s/__THIS_IS_THE_MOON_PHASE__/$MOON_PHASE/g" < PRE-moon.html > moon.html
 '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' --headless --disable-gpu --screenshot=moon.png moon.html
 
 #Parse Weather and replace placeholder text in the svg template file
