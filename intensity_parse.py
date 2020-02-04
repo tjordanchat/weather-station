77119762ef52fbbaee18c231b232d4862125c2d7
James Jordan <jamestjordan@gmail.com>
2020-02-04 09:17:32 -0500

    Tue Feb  4 08:57:40 EST 2020

--- intensity_parse.py
+++ intensity_parse.py
@@ -25,6 +25,9 @@ for x in range(0, 24):
     xcalc=str(int(offset+(width*x)))
     output = output.replace('XX_'+str(x)+"_", xcalc)
     rain_intens=data["hourly"]["data"][x]["precipIntensity"]
+    intensity=str(float(rain_intens)/2.0)
+    output = output.replace('FILL_OPAC_'+str(x)+"_", str(intensity))
+"""
     if rain_intens > 2:
        intensity="100"
     elif rain_intens > 0.3:
@@ -35,9 +38,7 @@ for x in range(0, 24):
         intensity=".20"
     else:
         intensity="0"
-
-    output = output.replace('FILL_OPAC_'+str(x)+"_", str(intensity))
-
+"""
 
 # ALL WORK DONE ABOVE -->
 codecs.open('intensity-processed.svg', 'w', encoding='utf-8').write(output)
