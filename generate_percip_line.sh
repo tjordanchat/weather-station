78aeb68789af5a2d0d3ab0ed66ff32c0057a2864
Tim Jordan <jamestjordan@gmail.com>
2021-06-28 13:38:42 -0400

    Mon Jun 28 13:26:59 EDT 2021

--- generate_percip_line.sh
+++ generate_percip_line.sh
@@ -1,7 +1,8 @@
 python percip_parse.py
 
+alias convert=/opt/homebrew/Cellar/imagemagick/7.0.11-13_2/bin/convert
 #convert svg to png, and rotate 90 degrees for horizontal view
-/usr/local/bin/convert -depth 8 -rotate 0 percip-processed.svg percip-processed.png
+convert -depth 8 -rotate 0 percip-processed.svg percip-processed.png
 
 #We optimize the image (necessary for viewing on the kindle)
 /usr/local/bin/pngcrush -q -c 0 percip-processed.png percip-script-output.png > /dev/null 2>&1
--- generate_sunrise.sh
+++ generate_sunrise.sh
@@ -2,8 +2,9 @@
 
 python sunrise_parse.py
 
+alias convert=/opt/homebrew/Cellar/imagemagick/7.0.11-13_2/bin/convert
 #convert svg to png, and rotate 90 degrees for horizontal view
-/usr/local/bin/convert -depth 8 -rotate 0 sunrise-processed.svg sunrise-processed.png
+convert -depth 8 -rotate 0 sunrise-processed.svg sunrise-processed.png
 
 #We optimize the image (necessary for viewing on the kindle)
 /usr/local/bin/pngcrush -q -c 0 sunrise-processed.png sunrise-script-output.png 2>/dev/null
